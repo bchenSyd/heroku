0dbd1abf3825d122316fc7d93f6a9b9b5b304367
Bo Chen <bochen2014@yahoo.com>
2017-10-08 17:10:36 +1100

    heroku

--- heroku.js
+++ heroku.js
@@ -2,7 +2,7 @@ import express from 'express';
 const port = process.env.port || 8000;
 
 const app = express();
-app.use(express.static('./'));
+app.use(express.static('./dist'));
 
 app.listen(port);
 console.log('server listening on http://localhost:'+port);
